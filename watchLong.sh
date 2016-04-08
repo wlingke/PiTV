cb0af62cb796ef6450e01c6d0f9281bc131d4cc0
Lingke <wlingke@gmail.com>
2016-04-07 22:15:22 -0700

    minor

--- watchLong.sh
+++ watchLong.sh
@@ -3,4 +3,4 @@
 echo "on 0" | cec-client -s
 echo "as" | cec-client -s
 sleep 7s
-livestreamer $1  best --player omxplayer --fifo --player-no-close
+livestreamer $1 best --player omxplayer --fifo
--- watchShort.sh
+++ watchShort.sh
@@ -3,6 +3,6 @@
 echo "on 0" | cec-client -s
 echo "as" | cec-client -s
 sleep 7s
-livestreamer $1  best --player omxplayer --fifo
+livestreamer $1 best --player omxplayer --fifo
 sleep 10s
 echo "standby 0" | cec-client -s
